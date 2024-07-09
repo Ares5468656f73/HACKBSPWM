48632517c8332439fec3e05316e6e0cc09c89190
Ares <erjesu099@proton.me>
2024-07-09 16:12:19 +0200

    changeTheme.sh was improve, now you can change the kitty and wallpapers in real time

---
+++ README.md
@@ -0,0 +1,115 @@
+# **Pentesting Environment on Kali Linux**
+
+This pentesting environment is specifically configured for Kali Linux and utilizes the following tools and configurations:
+
+| Window Manager | bspwm |
+| --- | --- |
+| Keybinding Manager  | sxhkd |
+| Terminal | kitty |
+| Terminal theme | powerlevel10k |
+| File Viewer | bat |
+| Directory Listing | lsd |
+
+![Untitled](README%20df198129275a48b0811c2b275cf58638/Untitled.jpeg)
+
+## Images
+
+---
+
+![Untitled](README%20df198129275a48b0811c2b275cf58638/Untitled.png)
+
+![Untitled](README%20df198129275a48b0811c2b275cf58638/Untitled%201.png)
+
+![Untitled](README%20df198129275a48b0811c2b275cf58638/Untitled%202.png)
+
+![Untitled](README%20df198129275a48b0811c2b275cf58638/Untitled%203.png)
+
+## **Installation**
+
+---
+
+To automatically configure the pentesting environment, use the `installer.sh` script included in this repository.
+
+### **Clone the Repository**
+
+Clone this repository to your local machine.
+
+```bash
+git clone https://github.com/Ares5468656f73/HACKBSPWM.git
+cd HACKBSPWM
+```
+
+Run the **installer.sh** script to install and configure all the necessary tools.
+
+```bash
+chmod +x installer.sh
+./installer.sh
+```
+
+After the script has finished, log out and log back in to apply all changes.
+
+## Change theme
+
+---
+
+To change the theme for your polybar and your kitty and also the background you only need to execute de [`changeTheme.sh`](http://changeTheme.sh) script. You will see this:
+
+![Untitled](README%20df198129275a48b0811c2b275cf58638/Untitled%204.png)
+
+The only thing yout need to do if put a parameter, for example if you want to chenge to Catppuccin theme you run:
+
+```bash
+./changeTheme.sh -c
+```
+
+To get the changes in your kitty you need to close and open a new terminal, after you log out, reboot or shutdown the machine the theme will apply
+
+## **Tool Details**
+
+---
+
+### **bspwm**
+
+bspwm is a tiling window manager that organizes windows in a binary tree format.
+
+### **sxhkd**
+
+sxhkd is a simple X hotkey daemon that maps key combinations to commands.
+
+### **kitty**
+
+kitty is a fast and feature-rich terminal emulator.
+
+### **powerlevel10k**
+
+powerlevel10k is a highly customizable zsh theme that provides useful information in the prompt.
+
+### **bat**
+
+bat is a cat clone with syntax highlighting and integrated pagination.
+
+### **lsd**
+
+lsd is a modern alternative to ls with a beautiful colorized output
+
+## **Credits**
+
+---
+
+The enviroment was configured by me :)
+
+The kitty and polybar colors are from [s4vitar](https://www.youtube.com/@s4vitar), [dracula](https://draculatheme.com/), [catppuccin](https://catppuccin.com/), [nvchad](https://nvchad.com/)
+
+Icons and fonts were from [HackNerdFonts](https://www.nerdfonts.com/)
+
+## **License**
+
+---
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+[https://github.com/Ares5468656f73/HACKBSPWM.git](https://github.com/Ares5468656f73/HACKBSPWM.git)
+
+[Ares](https://github.com/Ares5468656f73/)
+
+This `README.md` file provides comprehensive information about the pentesting environment, how to install it using the `installer.sh` script, a brief description of each tool included, and the contents of the `installer.sh` script itself.
--- changeTheme.sh
+++ changeTheme.sh
@@ -53,6 +53,58 @@ changeToMain ()
   alert = #f7768e
   disabled = #707880
   " > ~/.config/polybar/colors.ini
+  echo " " > ~/.config/kitty/color.ini
+
+  echo "
+  cursor_shape          Underline
+  cursor_underline_thickness 1
+  window_padding_width  20
+
+  # Special
+  foreground #a9b1d6
+  background #1a1b26
+
+  # Black
+  color0 #414868
+  color8 #414868
+
+  # Red
+  color1 #f7768e
+  color9 #f7768e
+
+  # Green
+  color2  #73daca
+  color10 #73daca
+
+  # Yellow
+  color3  #e0af68
+  color11 #e0af68
+
+  # Blue
+  color4  #7aa2f7
+  color12 #7aa2f7
+
+  # Magenta
+  color5  #bb9af7
+  color13 #bb9af7
+
+  # Cyan
+  color6  #7dcfff
+  color14 #7dcfff
+
+  # White
+  color7  #c0caf5
+  color15 #c0caf5
+
+  # Cursor
+  cursor #c0caf5
+  cursor_text_color #1a1b26
+
+  # Selection highlight
+  selection_foreground #7aa2f7
+  selection_background #28344a
+  " > ~/.config/kitty/color.ini
+
   bspc wm -r 
 }
 
@@ -71,7 +123,70 @@ changeToDracula ()
   disabled = #44475a
   " > ~/.config/polybar/colors.ini
   
+  echo " " > ~/.config/kitty/color.ini
+  echo "
+  # https://draculatheme.com/kitty
+
+  foreground            #f8f8f2
+  background            #282a36
+  selection_foreground  #ffffff
+  selection_background  #44475a
+
+  url_color #8be9fd
+
+  # black
+  color0  #21222c
+  color8  #6272a4
+
+  # red
+  color1  #ff5555
+  color9  #ff6e6e
+
+  # green
+  color2  #50fa7b
+  color10 #69ff94
+
+  # yellow
+  color3  #f1fa8c
+  color11 #ffffa5
+
+  # blue
+  color4  #bd93f9
+  color12 #d6acff
+
+  # magenta
+  color5  #ff79c6
+  color13 #ff92df
+
+  # cyan
+  color6  #8be9fd
+  color14 #a4ffff
+
+  # white
+  color7  #f8f8f2
+  color15 #ffffff
+
+  # Cursor colors
+  cursor            #f8f8f2
+  cursor_text_color background
+
+  # Tab bar colors
+  active_tab_foreground   #282a36
+  active_tab_background   #f8f8f2
+  inactive_tab_foreground #282a36
+  inactive_tab_background #6272a4
+
+  # Marks
+  mark1_foreground #282a36
+  mark1_background #ff5555
+
+  # Splits/Windows
+  active_border_color #f8f8f2
+  inactive_border_color #6272a4
+  " > ~/.config/kitty/color.ini
+  
   bspc wm -r
+  feh --bg-fill ~/Wallpapers/Dracula_Ship.png
 }
 
 changeToDark ()
@@ -89,7 +204,60 @@ changeToDark ()
   disabled = #42464e 
   " > ~/.config/polybar/colors.ini
   
+  echo " " > ~/.config/kitty/color.ini
+
+  echo "
+  cursor_shape          Underline
+  cursor_underline_thickness 1
+  window_padding_width  20
+
+  # Special
+  foreground #42464e
+  background #1e222a
+
+  # Black
+  color0 #1e222a
+  color8 #1e222a
+
+  # Red
+  color1 #e06c75
+  color9 #e06c75
+
+  # Green
+  color2  #98c379
+  color10 #98c379
+
+  # Yellow
+  color3  #e5c07b
+  color11 #e5c07b
+
+  # Blue
+  color4  #61afef
+  color12 #61afef
+
+  # Magenta
+  color5  #c678dd
+  color13 #c678dd
+
+  # Cyan
+  color6  #56b6c2
+  color14 #56b6c2
+
+  # White
+  color7  #abb2bf
+  color15 #abb2bf
+
+  # Cursor
+  cursor #c0caf5
+  cursor_text_color #1a1b26
+
+  # Selection highlight
+  selection_foreground #7aa2f7
+  selection_background #28344a
+  " > ~/.config/kitty/color.ini
+
   bspc wm -r 
+  feh --beg-fill ~/Wallpapers/Dark_theme.png
 }
 
 changeToCatppuccin ()
@@ -107,7 +275,62 @@ changeToCatppuccin ()
   disabled = #6c6f85
   " > ~/.config/polybar/colors.ini
   
+  echo " " > ~/.config/kitty/color.ini 
+
+  echo " 
+  cursor_shape          Underline
+  cursor_underline_thickness 1
+  window_padding_width  20
+
+  # Special
+  foreground #5C5F77
+  background #24273A
+
+  # Black
+  color0 #24273A
+  color8 #24273A
+
+  # Red
+  color1 #ed8796
+  color9 #ed8796
+
+  # Green
+  color2  #a6da95
+  color10 #a6da95
+
+  # Yellow
+  color3  #eed49f
+  color11 #eed49f
+
+  # Blue
+  color4  #7dc4e4
+  color12 #7dc4e4
+
+  # Magenta
+  color5  #c6a0f6
+  color13 #c6a0f6
+
+  # Cyan
+  color6  #8bd5ca
+  color14 #8bd5ca
+
+  # White
+  color7  #cad3f5
+  color15 #cad3f5
+
+  # Cursor
+  cursor #c0caf5
+  cursor_text_color #1a1b26
+
+  # Selection highlight
+  selection_foreground #7aa2f7
+  selection_background #28344a
+  
+
+  " > ~/.config/kitty/color.ini 
+
   bspc wm -r 
+  feh --bg-fill ~/Wallpapers/PinkFloyd_catppuccin.jpg
 }
 
 parameter_counter=0
