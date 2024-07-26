cc58154dbe18912be7fd87a5aa5f3aad1b7fb802
Ares <erjesu099@proton.me>
2024-07-26 17:07:23 +0200

    V2.0 of setup.sh improved

--- setup.sh
+++ setup.sh
@@ -1,31 +1,27 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Colors
-COLORS=(
-  "green=\e[0;32m\033[1m"
-  "end=\033[0m\e[0m"
-  "red=\e[0;31m\033[1m"
-  "blue=\e[0;34m\033[1m"
-  "yellow=\e[0;33m\033[1m"
-  "purple=\e[0;35m\033[1m"
-  "turquoise=\e[0;36m\033[1m"
-  "gray=\e[0;37m\033[1m"
+declare -A COLORS=(
+  [green]="\e[0;32m\033[1m"
+  [end]="\033[0m\e[0m"
+  [red]="\e[0;31m\033[1m"
+  [blue]="\e[0;34m\033[1m"
+  [yellow]="\e[0;33m\033[1m"
+  [purple]="\e[0;35m\033[1m"
+  [turquoise]="\e[0;36m\033[1m"
+  [gray]="\e[0;37m\033[1m"
 )
 
-for color in "${COLORS[@]}"; do
-  eval $color
-done
-
 help_panel() {
-  echo -e "\t\t${green}-i)${end} ${gray}Install the environment${end}"
-  echo -e "\t\t${red}-r)${end} ${gray}Remove the environment${end}\n"
+  echo -e "\t\t${COLORS[green]}-i)${COLORS[end]} ${COLORS[gray]}Install the environment${COLORS[end]}"
+  echo -e "\t\t${COLORS[red]}-r)${COLORS[end]} ${COLORS[gray]}Remove the environment${COLORS[end]}\n"
 }
 
 user=$(whoami)
 dotfiles_path=$(find / -name "HACKBSPWM" 2>/dev/null)
 
 if [ -z "$dotfiles_path" ]; then
-  echo -e "${red}Error: HACKBSPWM directory not found.${end}"
+  echo -e "${COLORS[red]}Error: HACKBSPWM directory not found.${COLORS[end]}"
   exit 1
 fi
 
@@ -34,7 +30,7 @@ install_environment() {
   local extra_packages="lsd bat"
   local os=$1
   local update_cmd install_cmd
-  
+
   case $os in
     1)
       update_cmd="sudo apt update && sudo apt upgrade -y"
@@ -49,13 +45,13 @@ install_environment() {
       install_cmd="sudo dnf install -y $packages"
       ;;
     *)
-      echo -e "${yellow}Invalid option${end}"
+      echo -e "${COLORS[yellow]}Invalid option${COLORS[end]}"
       exit 1
       ;;
   esac
 
   if [ "$user" == "root" ]; then
-    echo -e "${red}You can't perform this operation as root.${end}"
+    echo -e "${COLORS[red]}You can't perform this operation as root.${COLORS[end]}"
     exit 1
   fi
 
@@ -78,7 +74,7 @@ remove_environment() {
   local packages="bspwm sxhkd kitty polybar rofi feh nmap zsh"
   local os=$1
   local update_cmd remove_cmd
-  
+
   case $os in
     1)
       update_cmd="sudo apt update && sudo apt upgrade -y"
@@ -93,13 +89,13 @@ remove_environment() {
       remove_cmd="sudo dnf remove -y $packages"
       ;;
     *)
-      echo -e "${yellow}Invalid option${end}"
+      echo -e "${COLORS[yellow]}Invalid option${COLORS[end]}"
       exit 1
       ;;
   esac
 
   if [ "$user" == "root" ]; then
-    echo -e "${red}You can't perform this operation as root.${end}"
+    echo -e "${COLORS[red]}You can't perform this operation as root.${COLORS[end]}"
     exit 1
   fi
 
@@ -131,7 +127,7 @@ install_fonts() {
 setup_zsh() {
   local zsh_custom="$HOME/.oh-my-zsh/custom"
   local plugins_dir="$zsh_custom/plugins"
-  
+
   if [ -f ~/.zshrc ]; then
     rm ~/.zshrc
   fi
@@ -153,16 +149,20 @@ setup_zsh() {
 while getopts "ir" arg; do
   case $arg in
     i)
-      echo -e "\n\t${green}Installing the environment${end}\n"
+      echo -e "\n\t${COLORS[green]}Installing the environment${COLORS[end]}\n"
       sleep 1.5
-      install_environment
-      echo -e "${green}\n\tThe environment has been installed, please reboot the machine\n${end}"
+      echo -e "${COLORS[yellow]}Please select your operating system:\n1) Debian/Ubuntu\n2) Arch\n3) Fedora${COLORS[end]}"
+      read -p "Enter the number corresponding to your OS: " os
+      install_environment $os
+      echo -e "${COLORS[green]}\n\tThe environment has been installed, please reboot the machine\n${COLORS[end]}"
       ;;
     r)
-      echo -e "\n\t${red}Removing the environment${end}\n"
+      echo -e "\n\t${COLORS[red]}Removing the environment${COLORS[end]}\n"
       sleep 1.5
-      remove_environment
-      echo -e "${red}\n\tThe environment has been removed, please reboot the machine\n${end}"
+      echo -e "${COLORS[yellow]}Please select your operating system:\n1) Debian/Ubuntu\n2) Arch\n3) Fedora${COLORS[end]}"
+      read -p "Enter the number corresponding to your OS: " os
+      remove_environment $os
+      echo -e "${COLORS[red]}\n\tThe environment has been removed, please reboot the machine\n${COLORS[end]}"
       ;;
     *)
       help_panel
