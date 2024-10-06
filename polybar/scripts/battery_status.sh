3dae764b49d852acee1bb38d67bb6a7dc6e41597
Ares <erjesu099@proton.me>
2024-10-06 13:56:52 +0200

    docs(battery_status.sh): basic docs

--- polybar/scripts/battery_status.sh
+++ polybar/scripts/battery_status.sh
@@ -3,11 +3,10 @@
 battery_percentage=$(upower -i $(upower -e | grep battery) | grep percentage | awk '{print $2}' | tr -d "%")
 battery_status=$(upower -i $(upower -e | grep battery) | grep state | awk '{print $2}')
 
-# Verifica si upower falló
 if [ -z "$battery_percentage" ] || [ -z "$battery_status" ]; then
-  echo "%{F#f7768e}󱉝 %{F-}"  # Muestra un ícono de error si no se obtiene información de la batería
+  echo "%{F#f7768e}󱉝 %{F-}"  # If battery doesnt exist
 elif [ "$battery_status" == "charging" ]; then
-  echo "%{F#73daca} $battery_percentage%%{F-}"  # Muestra el icono de carga
+  echo "%{F#73daca} $battery_percentage%%{F-}"  # if it is charging
 else
-  echo "%{F#7dcfff} $battery_percentage%%{F-}"  # Muestra el porcentaje cuando no está cargando
+  echo "%{F#7dcfff} $battery_percentage%%{F-}"  # normal percentage
 fi
